bad1830c166a7e80c13ac760bfcb81775e82f889
)- <a-@-s-MacBook-Pro.local>
2022-07-17 03:42:57 -0400

    templates added, we can now view home and about page

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,11 @@
-from django.http import HttpRequest
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('home Page')
+    return render(request,'home.html')
 
 
 def about(request):
-    return HttpResponse('About Page')
+    return render(request, 'about.html')
