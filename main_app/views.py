fcfe35ab260e4b2887c45c73d30abc308e98b82e
)- <a-@-s-MacBook-Pro.local>
2022-07-17 12:07:15 -0400

    seed data added for rendering purposes

--- main_app/views.py
+++ main_app/views.py
@@ -9,3 +9,22 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+#Seed Data
+class Item:
+    def __init__(self, name, price, qty):
+        self.name = name
+        self.price = price
+        self.qty = qty
+
+items = [
+    Item('Drill', '$8.99', 3),
+    Item('TV', '$20.99', 7),
+    Item('Car', '$99.99', 10),
+    Item('Laptop', '$10.99', 3),
+]
+
+def items_index(request):
+   #item = Item.objects.all()
+    return render(request, 'items/index.html', {'items': items})
